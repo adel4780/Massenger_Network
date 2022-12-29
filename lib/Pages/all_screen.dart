461f392376ec2f42e9fd69a4888570493feebcdb
adel4780 <77065489+adel4780@users.noreply.github.com>
2022-12-29 12:16:01 +0330

    Update all_screen.dart

--- lib/Pages/all_screen.dart
+++ lib/Pages/all_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:massenger/chatmodel/chat_model.dart';
 import 'package:massenger/Pages/single_chatscreen.dart';
-import 'package:massenger/chatmodel/chat_model.dart';
 class AllScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
