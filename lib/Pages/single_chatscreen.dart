7d0dce0abc2946343a56b62b3e245cf7fc3fb05b
adel4780 <77065489+adel4780@users.noreply.github.com>
2022-12-29 10:47:53 +0330

    Update single_chatscreen.dart
    Socket_io is coppied to single_chatscreen

--- lib/Pages/single_chatscreen.dart
+++ lib/Pages/single_chatscreen.dart
@@ -1,70 +1,140 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:massenger/massenger_home.dart';
-import 'package:massenger/chatmodel/chat_model.dart';
+import '../chatmodel/chat_model.dart';
+class SingleChatScreen extends StatefulWidget {
+  final ChatModel data ;
+  final int idx;
+  const SingleChatScreen({super.key, required this.data, required this.idx, });
+  @override
+  State<SingleChatScreen> createState() => _SingleChatScreen();
+}
 
-class SingleChatScreen extends StatelessWidget {
-  final ChatModel data;
-
-  const SingleChatScreen({super.key, required this.data});
-
+class _SingleChatScreen extends State<SingleChatScreen> {
+  late TextEditingController textcontroller;
+  List<ChatModel> messages = [];
+  late int userId;
+  @override
+  void initState() {
+    super.initState();
+    // TODO: implement initState
+    textcontroller = TextEditingController();
+    messages.add(widget.data);
+    // Erfan
+    // UserID bara inke mak payam midam ya on payam mide
+    userId = Random().nextInt(1000);
+  }
+  @override
+  void dispose(){
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
+        //title: const Text("Chat"),
         title: Row(
           children: [
             GestureDetector(
               onTap: () => Navigator.pop(context),
               child: const Icon(Icons.arrow_back),
             ),
-            const SizedBox(
-              width: 10,
-            ),
+            const SizedBox(width: 10,),
             const CircleAvatar(
               backgroundColor: Colors.grey,
+              //backgroundImage: ,
             ),
-            const SizedBox(
-              width: 10,
-            ),
-            Text(
-              data.name,
-              style: const TextStyle(fontSize: 16),
-            ),
+            const SizedBox(width: 10,),
+            Text(widget.data.name, style:const TextStyle(fontSize: 16),),
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Chat Page${data.name}",
-              style: const TextStyle(fontSize: 20),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('img/chat.jpg'),
+                fit: BoxFit.fill,
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // یک پیج بساز
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const MassengerHome()));
-                  },
-                  child: const Text("Second Page"),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10,left: 10,top: 10),
+                  child: ListView.builder(
+                      itemCount: messages.length,
+                      itemBuilder: (BuildContext context, int index){
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 5),
+                          padding: const EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
+                          decoration: BoxDecoration(
+                            // UserID bara inke mak payam midam ya on payam mide
+                              color: userId == messages[index].id
+                                  ? Colors.blueAccent
+                                  : Colors.white
+                          ),
+                          child: Row(
+                            children: [
+                              Text(messages[index].message),
+                            ],
+                          ),
+                        );
+                      }
+                  ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Back"),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
                 ),
-              ],
-            ),
-          ],
-        ),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        if(textcontroller.text.isNotEmpty){ // check
+                          String msg = textcontroller.text;
+                          setState(() {
+                            messages.add(ChatModel(
+                              id: userId,
+                              name: '',
+                              message: msg,
+                              time: '', // ?
+                              avatarUrl: '',
+                            )
+                            );
+                          });
+                          textcontroller.text = '';
+                        }
+                      },
+                      icon: const Icon(Icons.insert_emoticon),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: textcontroller,
+                        decoration: const InputDecoration(
+                          hintText: "type",
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // Erfan Ersal Payam ba soocket
+                      },
+                      icon: const Icon(Icons.send),
+                    ),
+                  ],
+                ),
+              ),
+
+            ],
+          ),
+        ],
       ),
     );
   }
